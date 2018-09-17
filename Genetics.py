10ea505a2530d272850280ff567536dba623c05b
abtheo <c.theo.kent@gmail.com>
2018-09-17 20:29:30 +0200

    iMap class instantiated

--- Genetics.py
+++ Genetics.py
@@ -84,64 +84,65 @@ class Spot:
         fitness = signalCounter / signalSizes.get(self.spotType) * 100
         return fitness
 
-class Allele:
+class iMap:
 
-    def __init__(self):               
-        self.spotList = generateSpots(SPOTNUM)
-#Returns list of newly generated spots
-def generateSpots(n):
-    newSpotList = list()
-    for i in range(n):
-        newSpot = Spot()
-        newSpotList.append(newSpot)
-    return newSpotList
+    def __init__(self,tilemap):               
+        self.spotList = self.generateSpots(SPOTNUM)
+        self.tilemap = tilemap
+        self.fitness = 0
+            
+    #Returns list of newly generated spots
+    def generateSpots(self,n):
+        newSpotList = list()
+        for i in range(n):
+            newSpot = Spot()
+            newSpotList.append(newSpot)
+        return newSpotList
 
-#Draws list of spots onto map
-def drawSpots(spotList,tilemap):
-    #Preserves original map
-    localMap = np.copy(tilemap)
-    for item in spotList:
-        localMap = item.draw(localMap)
-    return localMap
+    #Draws list of spots onto map
+    def drawSpots(self):
+        #Preserves original map
+        for item in self.spotList:
+            self.tilemap = item.draw(self.tilemap)
+    
+    #Evaluates percentage of white covered
+    #So needs a copy of original map (throughout generations)    
+    def mapFitness(self):
+        #Amount of open space in original map
+        tilemapArea = (self.tilemap == NONE).sum()
+        #Draws Wifi spots onto map
+        self.drawSpots()
+        #Amount of open space after spots drawn
+        spotMapArea = (self.tilemap == NONE).sum()
+        
+        #Gets overall coverage for fitness calculation
+        #Equates to percentage of free space now covered by signal
+        signalFitness = 100 - (spotMapArea / tilemapArea * 100)
+        
+        #Gets average fitness of each allele
+        alleleFitness = 0
+        for spot in self.spotList:
+            alleleFitness = (alleleFitness + spot.getFitness(self.tilemap)) / 2
+            
+        self.fitness = signalFitness + alleleFitness
+    
 
 #Generates N random population of spotMaps
 #Also gets fitness of each map
 def generatePopulation(N,tilemap):
     population = list()
     for i in range(N):
-        spotList = generateSpots(SPOTNUM)
-        population.append(mapFitness(spotList,tilemap))
+        newiMap = iMap(tilemap)
+        newiMap.mapFitness()
+        population.append(newiMap)
     return population
-    
-#Evaluates percentage of white covered
-#So needs a copy of original map (throughout generations)    
-def mapFitness(spotList, tilemap):
-    #Amount of open space in original map
-    tilemapArea = (tilemap == NONE).sum()
-    #Draws Wifi spots onto map
-    spotMap = drawSpots(spotList,tilemap)
-    #Amount of open space after spots drawn
-    spotMapArea = (spotMap == NONE).sum()
-    
-    #Gets overall coverage for fitness calculation
-    #Equates to percentage of free space now covered by signal
-    signalFitness = 100 - (spotMapArea / tilemapArea * 100)
-    
-    #Gets average fitness of each allele
-    alleleFitness = 0
-    for spot in spotList:
-        alleleFitness = (alleleFitness + spot.getFitness(spotMap)) / 2
-        
-    fitness = signalFitness + alleleFitness
-    print(fitness)
-    return fitness, spotList, spotMap
 
 
 def pickParents(population):
     pool = []
     for i, imap in enumerate(population):
         #Fills pool with proportional percentage of each parent
-        proportion = np.full(np.rint(imap[0]),i,dtype="int32")
+        proportion = np.full(np.rint(imap.fitness),i,dtype="int32")
         pool = np.concatenate(  (pool, proportion)  ,axis=0)
     #Selects parents from pool
     mommy = int(np.random.choice(pool))
@@ -149,12 +150,12 @@ def pickParents(population):
     
     return population[mommy], population[daddy] 
 
-def reproduce(mommy, daddy):
+#def reproduce(mommy, daddy):
     
 
 #DISPLAYSURF = Maps.uiInit()
 #Generates new random map for algorithm to run inside    
 tilemap = Maps.createMap() 
 population = generatePopulation(10,tilemap)
-print(pickParents(population))
+print(pickParents(population)[0].tilemap)
 #Maps.uiRefresh(population[0][1], DISPLAYSURF)
